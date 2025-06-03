38a1135fc80786949780c329049630c41e5c2ce2
Soham <sohamkundu4002@gmail.com>
2025-06-03 22:28:03 +0530

    path change

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     
     dependencies {
-        classpath("com.android.tools.build:gradle:8.10.0")
+        classpath("com.android.tools.build:gradle:8.10.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.google.gms:google-services:4.4.0")
     }
